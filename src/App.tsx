960f813b2cc052d90cb4f4bee0f48991a9917688
DariusTans <101817395+DariusTans@users.noreply.github.com>
2025-01-08 16:05:21 +0700

    fix: update process .env

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,9 @@ function App(): ReactElement {
   const [formData, setFormData] = useState<FormData>({ name: '', description: '', files: [] });
   const [knowledgeData, setKnowledgeData] = useState<Knowledge[]>([]);
 
-  const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000';
+  const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8001';
+  console.log("process.env", process.env.REACT_APP_API_BASE_URL);
+  console.log("API_BASE_URL", API_BASE_URL);
 
   useEffect(() => {
     const fetchData = async () => {
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv('', process.cwd(), '');
+    return {
+        plugins: [react()],
+        define: {
+            'process.env.REACT_APP_API_BASE_URL': JSON.stringify(env.API_BASE_URL),
+            'process.env.YOUR_BOOLEAN_VARIABLE': env.YOUR_BOOLEAN_VARIABLE,
+            // If you want to exposes all env variables, which is not recommended
+            // 'process.env': env
+        },
+    };
+});
+// export default defineConfig({
+//   plugins: [react()]
+//   const env = loadEnv('', process.cwd());
+
+// })
